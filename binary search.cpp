4d9810a01bb669696c0a8dc1fd92f0f782727f58
samira186 <samiraparvin69@gmail.com>
2026-02-27 23:00:14 +0600

    binary search

---
+++ binary search.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, key;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter sorted elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter value to search: ";
+    cin >> key;
+
+    int low = 0, high = n - 1, mid;
+
+    while(low <= high) {
+        mid = (low + high) / 2;
+
+        if(arr[mid] == key) {
+            cout << "Element found at position " << mid + 1;
+            return 0;
+        }
+        else if(arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    cout << "Element not found";
+
+    return 0;
+}
